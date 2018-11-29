d674f1a129a1d2e8484326b162396ba16b781385
Mehran <36104315+Mehran-at@users.noreply.github.com>
2018-11-29 10:33:59 +0100

    Update AdvancedIndexApplication.java

--- src/AdvancedIndexApplication.java
+++ src/AdvancedIndexApplication.java
@@ -1,3 +1,22 @@
+// Matilda just bought a new book about social impact and as she opens it, she sees the index
+// . Create the AdvancedIndexApplication class and write the following instructions in its main method:
+// . Create the Topic class that has a name.
+// . Create an empty index map connecting Integer for the page numbers and a List of Topic for the several book topics that will appear in each page and display it.
+// . Create two Integers that represent two different page numbers.
+// . Create three Topic objects.
+// . Create two lists: topics1 and topics2.
+// . Add one Topic to topics1 and two into topics2.
+// . Put the page numbers and topics1 and topics2 respectively into the index map .
+// . Display the map keys. To display the Topic class create the toString method.
+// . Display the map values. To display the Topic class create the toString method.
+// . Create another different Topic.
+// . Get the topics1 from the map and call it stored. Add to stored the last Topic you created. Display the map.
+// . Ask if one of the keys is contained in the map and display the answer.
+// . Get the topics2 from the map and call it stored2 and display it.
+// . Display its size.
+//    Hints:
+//   >Use Intellij shortcuts to create the toString and equals methods.
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
